3de785690aee92231885acc5a366bd350d141b2e
takashi823 <uchimura.takashi724@gmail.com>
2024-02-03 22:02:34 +0900

    fix: デザイン修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
-import Image from "next/image";
-
-import { css } from "@emotion/css";
 import Chat from "@/components/Chat";
-
-const headerStyle = css`
-  padding: 15px;
-  background-color: #5c6bc0;
-  color: white;
-  text-align: center;
-  font-size: 24px;
-  font-weight: bold;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-`;
 
 export default function Home() {
   return (
-    <div>
-      <header className={headerStyle}>Gemini Chatbot</header>
+    <div className="flex flex-col min-h-screen">
+      <header className="static p-15 bg-blue-300 text-white text-center text-xl font-bold shadow-slate-100">Gemini Chatbot</header>
       <Chat />
     </div>
   );
--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import { css } from "@emotion/css";
 import axios from "axios";
 import ReactMarkdown from "react-markdown";
 
@@ -13,86 +12,19 @@ interface Part {
   text: string;
 }
 
-const chatContainerStyle = css`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  background-color: #f5f5f5;
-`;
-
-const chatHistoryStyle = css`
-  overflow-y: auto;
-  flex-grow: 1;
-  padding: 20px;
-  margin-bottom: 60px;
-`;
-
-const userMessageStyle = css`
-  margin-bottom: 10px;
-  padding: 10px;
-  background-color: #e1f5fe;
-  border-radius: 10px;
-  max-width: 70%;
-  align-self: flex-end;
-`;
-
-const botMessageStyle = css`
-  margin-bottom: 10px;
-  padding: 10px;
-  background-color: #ede7f6;
-  border-radius: 10px;
-  max-width: 70%;
-  align-self: flex-start;
-`;
-
-const inputStyle = css`
-  flex: 1;
-  padding: 10px 15px;
-  font-size: 16px;
-  border: 2px solid #dedede;
-  border-radius: 4px;
-  margin-right: 10px;
-`;
-
-const buttonStyle = css`
-  padding: 10px 20px;
-  background-color: #5c6bc0;
-  color: white;
-  font-size: 16px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  &:hover {
-    background-color: #3949ab;
-  }
-`;
-
-const inputAreaStyle = css`
-  display: flex;
-  justify-content: space-between;
-  padding: 10px;
-  background-color: #fafafa;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  box-shadow: 0 -1px 10px rgba(0, 0, 0, 0.1);
-`;
-
 const Chat = () => {
-  const [input, setInput] = useState<string>("");
+  const [question, setQuestion] = useState<string>("");
   const [chatHistory, setChatHistory] = useState<ChatMessage[]>([]);
 
   const sendMessage = async () => {
-    const userMessage: ChatMessage = { role: "user", content: input };
-    // 画面上の会話履歴を更新
+    const userMessage: ChatMessage = { role: "user", content: question };
     const updatedChatHistory = [...chatHistory, userMessage];
 
     try {
       const response = await axios.post(
         `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`,
         {
-          contents: [{ parts: [{ text: input }] }], // 現在のメッセージのみを送信
+          contents: [{ parts: [{ text: question }] }], 
         },
         {
           headers: {
@@ -101,16 +33,15 @@ const Chat = () => {
         }
       );
 
-      // APIからの応答を処理
-      const botResponse = response.data;
+      const chatAnswer = response.data;
       let botMessageContent = "";
 
       if (
-        botResponse &&
-        botResponse.candidates &&
-        botResponse.candidates.length > 0
+        chatAnswer &&
+        chatAnswer.candidates &&
+        chatAnswer.candidates.length > 0
       ) {
-        const firstCandidate = botResponse.candidates[0].content;
+        const firstCandidate = chatAnswer.candidates[0].content;
         if (
           firstCandidate &&
           firstCandidate.parts &&
@@ -127,45 +58,48 @@ const Chat = () => {
         content: botMessageContent,
       };
 
-      // 会話履歴を更新（ユーザーとボットのメッセージを含む）
       setChatHistory([...updatedChatHistory, botMessage]);
     } catch (error) {
       console.error("Google API error:", error);
     }
 
-    setInput("");
+    setQuestion("");
   };
 
   const renderChatMessage = (message: ChatMessage) => {
     if (message.role === "system") {
-      // マークダウン形式のメッセージをHTMLに変換して表示
       return <ReactMarkdown>{message.content}</ReactMarkdown>;
     }
-    return <div>{message.content}</div>; // 通常のテキストメッセージ
+    return <div>{message.content}</div>;
   };
 
   return (
-    <div className={chatContainerStyle}>
-      <div className={chatHistoryStyle}>
+    <div className="flex flex-col h-full">
+      <div className="overflow-y-auto grow p-5 mb-3">
         {chatHistory.map((chat, index) => (
           <div
             key={index}
-            className={
-              chat.role === "user" ? userMessageStyle : botMessageStyle
-            }
+            className={`p-4 my-3 rounded-md ${
+              chat.role === "user"
+                ? "bg-blue-100 text-black"
+                : "bg-red-100 text-gray-800"
+            }`}
           >
             {renderChatMessage(chat)}
           </div>
         ))}
       </div>
-      <div className={inputAreaStyle}>
+      <div className="flex space-x-0 p-2 bg-white fixed bottom-0 left-0 right-0 shadow">
         <input
           type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          className={inputStyle}
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+          className="flex-1 py-2 px-3 text-base rounded-sm outline-none focus:outline-blue-800 outline-blue-300 mr-3"
         />
-        <button onClick={sendMessage} className={buttonStyle}>
+        <button
+          onClick={sendMessage}
+          className="py-5 px-8 bg-blue-800 text-white text-base rounded hover:bg-blue-300 cursor-pointer"
+        >
           Send
         </button>
       </div>
